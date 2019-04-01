12061f294fdb39736be43b22c24fa58e82e4cb89
Jonathan Friedberg <friedberg_jon@hotmail.com>
2019-04-01 21:19:58 +0100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,22 +1,22 @@
 var cards = ["queen", "queen", "king", "king"];
-var cardsInPlay = []
+var cardsInPlay = [];
 
 var cardOne = cards[0];
-cardsInPlay.push('cardOne');
-console.log("User flipped queen");
+cardsInPlay.push(cards);
 
-var cardTwo = cards[1];
-cardsInPlay.push('cardTwo');
-console.log("User flipped king");
+console.log ("User flipped " + cards[0]);
 
-if (cardsInPlay.length === 2)
+var cardTwo = cards[2];
+cardsInPlay.push(cards);
+console.log ("User flipped " + cards[2]);
 
-{if (cardsInPlay[0] === cardsInPlay[2])
-	{
-		alert("you found a match!")
-	} else {
-		alert("Sorry, try again.")
-	}
-};
-	
+if (cardsInPlay.length === 2){
+	if (cardsInPlay[0] === cardsInPlay[2])
+{
+console.log ("You found a match!")
+}
+else {
+	console.log ("Sorry, try again")
+}};
+
 
